dc96ce8aa3c39292a5a78d678131ff1560ce406e
Ruben Burgos <code.rburgos@gmail.com>
2022-11-30 10:02:01 -0600

    Add project files.

---
+++ TDEE_BMR_Macro_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TDEE_BMR_Macro_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
